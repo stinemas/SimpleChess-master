86935d4c2c5bed031d14d578cd243612fef2e0c1
stinemas <63400786+stinemas@users.noreply.github.com>
2021-09-07 13:53:43 +0200

    Initial commit

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChess
+{
+    public class Bishop : Piece
+    {
+        public Bishop() : base("LPR")
+        {
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+    }
+}
